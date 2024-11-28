3585da070283d61e86f5e1bd27942fc33fcc0166
José Bocalom <84545247+JLBocalom@users.noreply.github.com>
2024-11-28 09:15:49 -0300

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function clearForm() {
-    document.getElementById('nome').value = '';
-    document.getElementById('telefone').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('senha').value = '';
-    document.getElementById('id').value = '';
-}
